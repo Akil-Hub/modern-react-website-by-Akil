df05771c49e8f367231cf27116d940cf17904d77
Akil-Hub <akilanjummahi@gmail.com>
2025-05-02 21:49:54 +0600

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Welcome to Akil's Custom React Template</h1>
+      <h1 className='text-4xl font-bold text-danger bg'>Welcome to Akil's Custom React Template</h1>
     </div>
   );
 };
